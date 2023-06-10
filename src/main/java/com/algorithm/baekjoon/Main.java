fa925650202d4efa8d6c206a8e2020145ea496c8
myeongbaek <moungbak421@daum.net>
2023-06-10 16:14:58 +0900

    echo 2023-06-10

--- src/main/java/com/algorithm/baekjoon/Main.java
+++ src/main/java/com/algorithm/baekjoon/Main.java
@@ -1,42 +1,24 @@
 package com.algorithm.baekjoon;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        int[] sidesOfTriangle = new int[3];
-        while (true) {
-            int max = 0;
-            int total = 0;
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < 3; i++) {
-                int sideLength = Integer.parseInt(st.nextToken());
-                sidesOfTriangle[i] = sideLength;
-                max = Math.max(max, sideLength);
-                total += sideLength;
-            }
-            if (max == 0) {
+        int N = Integer.parseInt(br.readLine());
+        int result = -1;
+        // 5 키로그램으로 최대한 많이 담는 경우에서부터 따져본다
+        for(int i = N / 5; i >= 0; i--){
+            if((N - 5 * i) % 3 == 0){
+                result = i + (N - 5 * i) / 3;
                 break;
             }
-            if (max >= total - max) {
-                bw.write("Invalid");
-            } else {
-                if (sidesOfTriangle[0] == sidesOfTriangle[1] && sidesOfTriangle[0] == sidesOfTriangle[2]) {
-                    bw.write("Equilateral");
-                } else if (sidesOfTriangle[0] == sidesOfTriangle[1] || sidesOfTriangle[0] == sidesOfTriangle[2] || sidesOfTriangle[1] == sidesOfTriangle[2]) {
-                    bw.write("Isosceles");
-                } else {
-                    bw.write("Scalene");
-                }
-            }
-            bw.newLine();
         }
+        bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
     }
 }
+
