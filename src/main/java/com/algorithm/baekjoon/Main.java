daa9393d4bc6782d4ed1233425b34aaa5cf37731
myeongbaek <moungbak421@daum.net>
2023-07-22 01:23:18 +0900

    2023-07-22

--- src/main/java/com/algorithm/baekjoon/Main.java
+++ src/main/java/com/algorithm/baekjoon/Main.java
@@ -1,7 +1,35 @@
 package com.algorithm.baekjoon;
 
-class Main {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
+class Main {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        String[][] array = new String[N][2];
+
+        // 입력
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            array[i][0] = st.nextToken();
+            array[i][1] = st.nextToken();
+        }
+
+        // 정렬
+        Arrays.sort(array, Comparator.comparingInt(o -> Integer.parseInt(o[0])));
+
+        // 출력
+        for (int i = 0; i < N; i++) {
+            bw.write(array[i][0] + " " + array[i][1]);
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
     }
 }
