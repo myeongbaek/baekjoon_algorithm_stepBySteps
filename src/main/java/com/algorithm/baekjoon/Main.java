0d5c24d54c6a3be1ad7cbae61a8098c4f9126d39
myeongbaek <moungbak421@daum.net>
2023-07-31 18:24:26 +0900

    2023-07-31

--- src/main/java/com/algorithm/baekjoon/Main.java
+++ src/main/java/com/algorithm/baekjoon/Main.java
@@ -1,9 +1,7 @@
 package com.algorithm.baekjoon;
 
 import java.io.*;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,26 +9,22 @@ class Main {
     public static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int cnt = 0;
+        int N = Integer.parseInt(br.readLine());
         Map<String, Integer> hm = new HashMap<>();
-        // 입력 & 검사
-        for(int i = 0; i < N + M; i++){
-            if(i < N){
-                hm.put(br.readLine(), 0);
+        for(int i = 0; i < N; i++){
+            String name = br.readLine().split(" ")[0];
+            if(!hm.containsKey(name)){
+                hm.put(name, 0);
             } else {
-                if(hm.containsKey(br.readLine())){
-                    cnt++;
-                }
+                hm.remove(name);
             }
         }
-        // 출력
-        bw.write(cnt + "");
-        bw.flush();
-        bw.close();
+
+        List<String> keyList = new ArrayList<>(hm.keySet());
 
 
+
+
+        for(String name : hm.key)
     }
 }
---
+++ src/main/java/com/algorithm/baekjoon/Test.java
@@ -0,0 +1,13 @@
+package com.algorithm.baekjoon;
+
+public class Test {
+    public static void main(String[] args) {
+        int total = 0;
+        while(true){
+            func();
+        }
+    }
+    public static void func(){
+
+    }
+}
