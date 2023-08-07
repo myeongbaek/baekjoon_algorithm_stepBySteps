eff5a804380e7381d4c2c1f186519b054d2ce979
myeongbaek <moungbak421@daum.net>
2023-08-07 23:28:23 +0900

    2023-08-07

--- src/main/java/com/algorithm/baekjoon/Main.java
+++ src/main/java/com/algorithm/baekjoon/Main.java
@@ -1,46 +1,25 @@
 package com.algorithm.baekjoon;
 
 import java.io.*;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static StringBuilder sb = new StringBuilder();
     public static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        LinkedList<Map<Integer, Integer>> linkedList = new LinkedList<>();
-        List<Integer> result = new LinkedList<>();
-        Map<Integer, Integer> hm;
+        // 1. 갯수 N
         int N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        // 2. 길이가 N인 수열 A에 대하여
+        st 
 
-        // 입력
-        for (int i = 1; i <= N; i++) {
-            hm = new HashMap<>();
-            hm.put(i, Integer.parseInt(st.nextToken()));
-            linkedList.offer(hm);
-        }
 
-        // 펑!
-        int index = 0, number;
-        while (!linkedList.isEmpty()) {
-            while(index < linkedList.size()){
-                index %= linkedList.size();
-            }
-            hm = linkedList.remove(index);
-            for (int balloon : hm.keySet()) {
-                result.add(balloon);
-                number = hm.get(balloon);
-                index += number > 0 ? number - 1 : number + linkedList.size();
-                break;
-            }
-        }
 
         // 출력
-        for(int x : result){
-            bw.write(x + " ");
-        }
         bw.flush();
         bw.close();
     }
---
+++ src/main/java/com/algorithm/baekjoon/ch16_10.java
@@ -0,0 +1,47 @@
+package com.algorithm.baekjoon;
+
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+class ch16_10 {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        Deque<int[]> deq = new ArrayDeque<>();
+
+        // 입력
+        for (int i = 0; i < N; i++) {
+            deq.add(new int[]{
+                    i + 1, // 풍선 idx
+                    Integer.parseInt(st.nextToken()) // 내용 숫자
+            });
+        }
+
+        // 펑!
+        while (deq.size() > 1) {
+            int[] balloon = deq.poll();
+            sb.append(balloon[0]).append(" ");
+            int move = balloon[1];
+            if(move < 0){ // 뒤로 돌리기
+                while(move++ < 0)
+                    deq.addFirst(deq.pollLast());
+            } else { // 앞으로 돌리기
+                move--;
+                while(move-- > 0)
+                    deq.addLast(deq.pollFirst());
+            }
+        }
+
+        // 출력
+        bw.write(sb.append(deq.poll()[0]).toString());
+        bw.flush();
+        bw.close();
+    }
+}
